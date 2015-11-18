842ec97e02df5b299a9a0ef8ddea0751b77a5445
Thejdeep <tejdeepg@gmail.com>
2015-11-18 13:38:56 +0530

    test2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello cloud comp');
+  res.send('hello cloud computing');
 });
  
 app.listen(process.env.PORT || 5000);
